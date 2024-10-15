37efe8cb4d4529ef25744a0080a1cfc2e90986e5
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-10-15 18:19:11 +0000

    project finalized

--- src/models.py
+++ src/models.py
@@ -8,84 +8,83 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+    __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
-    user_name=Column(String(250))
-    first_name=Column(String(250))
-    last_name=Column(String(250))
-    email=Column(String(250))
-    password=Column(String(250))
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), unique=True)
+    password = Column(String(250))
+    subscription_date = Column(Integer)
 
-    # name = Column(String(250), nullable=False)
+    favorite=relationship("Favorite")
 
-class Favorites(Base):
-    __tablename__ = 'Favorites'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite(Base):
+
+    __tablename__ = 'favorite'
+
     id = Column(Integer, primary_key=True)
-    Characters=Column(String(250))
-    Planets=Column(String(250))
-    Vechicles=Column(String(250))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id')) 
 
-class Characters(Base):
-    __tablename__ = 'Characters'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    user_id=Column(Integer, ForeignKey('user.id')) 
+
+
+class Character(Base):
+
+    __tablename__ = 'character'
+
     id = Column(Integer, primary_key=True)
-    name=Column(String(250))
-    height=Column(String(250))
-    mass=Column(String(250))
-    hair_color=Column(String(250))
-    skin_color=Column(String(250))
-    eye_color=Column(String(250))
-    birth_year=Column(String(250))
-    gender=Column(String(250))
-    
-    # person_id = Column(Integer, ForeignKey('person.id'))
-    # person = relationship(Person)
+    name = Column(String(250))
+    height = Column(String(250))
+    mass = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
 
-class Planets(Base):
-    __tablename__ = 'Planets'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favorite=relationship("Favorite")
+
+class Planet(Base):
+
+    __tablename__ = 'planet'
+
     id = Column(Integer, primary_key=True)
-    name=Column(String(250))
-    rotation=Column(String(250))
-    orbital=Column(String(250))
-    diameter=Column(String(250))
-    climate=Column(String(250))
-    gravity=Column(String(250))
-    terrain=Column(String(250))
-    surface_water=Column(String(250))
-    population=Column(String(250))
+    name = Column(String(250))
+    rotation = Column(String(250))
+    orbital = Column(String(250))
+    diameter = Column(String(250))
+    climate = Column(String(250))
+    gravity = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+    population = Column(String(250))
 
-    # person_id = Column(Integer, ForeignKey('person.id'))
-    # person = relationship(Person)
+    favorite=relationship("Favorite")
 
+class Vehicle(Base):
 
-class Vehicles(Base):
-    __tablename__ = 'Vehicles'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    __tablename__ = 'vehicle'
+
     id = Column(Integer, primary_key=True)
-    name= Column(String(250))
-    model=Column(String(250))
-    manufacturer=Column(String(250))
-    cost_in_credits=Column(String(250))
-    length=Column(String(250))
-    max_atmosphering_speed=Column(String(250))
-    crew=Column(String(250))
-    passengers=Column(String(250))
-    cargo_capacity=Column(String(250))
-    consumables=Column(String(250))
-    vehicle_class=Column(String(250))
+    name = Column(String(250))
+    model = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(String(250))
+    length = Column(String(250))
+    max_atmosphering_speed = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+    vehicle_class = Column(String(250))
 
-    # person_id = Column(Integer, ForeignKey('person.id'))
-    # person = relationship(Person)
-
+    favorite=relationship("Favorite")
 
 
-## Draw from SQLAlchemy base
+# Generar diagrama
 render_er(Base, 'diagram.png')
